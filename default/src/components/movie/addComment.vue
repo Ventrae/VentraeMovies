065090e695a97d24dde1305972a6f2590165aeb8
Filip Dobrzyniewicz <ventrae@gmail.com>
2020-01-26 18:43:02 +0100

    Created input for comment

--- default/src/components/movie/addComment.vue
+++ default/src/components/movie/addComment.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="bg-warning col-12 col-md-6 mx-auto fixed-bottom row">
-        <mdb-input type="textarea" label="Dodaj komentarz" outline :rows="2" class="col-12 col-md-10"/>
+    <div class="bg-white col-12 col-md-6 mx-auto fixed-bottom row">
+        <div class="col-11 col-md-8">
+            <mdb-input type="textarea" label="Dodaj komentarz" outline :rows="2"/>
+        </div>
+        <div class="col-4 col-md-2">
+            <mdb-btn color="primary" class="d-flex">Dodaj <i class="fas fa-chevron-right"></i></mdb-btn>
+        </div>
     </div>
 </template>
 
